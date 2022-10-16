19177bc649f41b41899c8776cf9cf3aeb373abfa
= <jraynovic@key.net>
2022-10-16 11:35:32 -0700

    refactor: moved modal close into more readable function

--- client/src/components/StepComponents/ModalComponent.js
+++ client/src/components/StepComponents/ModalComponent.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Modal, ModalBody } from 'reactstrap';
 
 function ModalComponent( {shown, setShown} ) {
+  const close = ()=> setShown({title:'',contents:''})
   return (
     <>
       <div>
-        <Modal isOpen={shown.title} toggle={()=>setShown({title:'',contents:''})}>
+        <Modal isOpen={shown.title} toggle={close}>
           {/* Header still needs styled and chevron added after icon lib is decided on */}
-          <div className='modalHeader'>{shown.title} <span style={{cursor:'pointer'}} onClick={()=>setShown({title:'',contents:''})}>X</span></div>
+          <div className='modalHeader'>
+            {shown.title} 
+            <span style={{cursor:'pointer'}} onClick={close}>X</span>
+          </div>
           <ModalBody>  
             {shown.contents}
           </ModalBody>
